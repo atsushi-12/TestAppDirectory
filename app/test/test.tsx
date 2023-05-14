f262178103aa7d1fefaf801ade7a4f1043d3e773
atsushi-12 <121540076+atsushi-12@users.noreply.github.com>
2023-05-15 00:58:04 +0900

    AppDirectoryでの開発

---
+++ app/test/test.tsx
@@ -0,0 +1,16 @@
+//fuctional component
+
+import React from "react";
+
+type Props = {
+  //props
+};
+const test = () => {
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+export default test;
